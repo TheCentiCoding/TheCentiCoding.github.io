974579271a9e395784591b9655259879b337d1e6
The CentiCoding <centicodes2007@gmail.com>
2025-11-26 21:03:53 +0700

    Delete cipher.js

--- cipher.js
+++
@@ -1,8 +0,0 @@
-const btn = document.getElementById("helloBtn");
-const msg = document.getElementById("message");
-
-if (btn && msg) {
-    btn.addEventListener("click", () => {
-        msg.textContent = "Halo! Kamu baru saja menekan tombol 🎉";
-    });
-}

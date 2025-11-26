0c08508ef57c7ccb09d61eab7b7a1b74f26e0fd1
The CentiCoding <centicodes2007@gmail.com>
2025-11-26 20:15:18 +0700

    Update cipher.js

--- cipher.js
+++ cipher.js
@@ -7,5 +7,13 @@ const decoded = document.getElementById("decoded");
 if (encodebutton && decoded) {
   encodebutton.addEventListener("commitEncode", () => {
       encoded.textContent = decoded.value;
+      document.getElementById("encodeLog").textContent = "Encoded!";
+    }
+}
+
+if (decodebutton && encoded) {
+  decodebutton.addEventListener("commitDecode", () => {
+      decoded.textContent = encoded.value;
+      document.getElementById("decodeLog").textContent = "Decoded!";
     }
 }
